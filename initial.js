79505e33283194110e9519d4299065db9a484a13
GoSum215 <85661196+GoSum215@users.noreply.github.com>
2021-07-29 14:25:19 +1000

    initialization + issuance of a random tile
    + installing the first tile

---
+++ initial.js
@@ -0,0 +1,34 @@
+data = '[{"id" : 0, "number" : 2, "routeN" : 1, "routeE" : 1, "routeS" : 2, "routeW" : 1, "isShield" : false, "isEndOfRoad" : true, "isEndOfTown" : false, "isMonastery" : true}, {"id" : 1, "number" : 4, "routeN" : 1, "routeE" : 1, "routeS" : 1, "routeW" : 1, "isShield" : false, "isEndOfRoad" : false, "isEndOfTown" : false, "isMonastery" : true}, {"id" : 2, "number" : 1, "routeN" : 3, "routeE" : 3, "routeS" : 3, "routeW" : 3, "isShield" : true, "isEndOfRoad" : false, "isEndOfTown" : false, "isMonastery" : false}, {"id" : 3, "number" : 3, "routeN" : 2, "routeE" : 3, "routeS" : 2, "routeW" : 1, "isShield" : false, "isEndOfRoad" : false, "isEndOfTown" : true, "isMonastery" : false}, {"id" : 4, "number" : 5, "routeN" : 3, "routeE" : 1, "routeS" : 1, "routeW" : 1, "isShield" : false, "isEndOfRoad" : false, "isEndOfTown" : true, "isMonastery" : false}, {"id" : 5, "number" : 2, "routeN" : 1, "routeE" : 3, "routeS" : 1, "routeW" : 3, "isShield" : true, "isEndOfRoad" : false, "isEndOfTown" : false, "isMonastery" : false}, {"id" : 6, "number" : 1, "routeN" : 3, "routeE" : 1, "routeS" : 3, "routeW" : 1, "isShield" : false, "isEndOfRoad" : false, "isEndOfTown" : false, "isMonastery" : false}, {"id" : 7, "number" : 3, "routeN" : 1, "routeE" : 3, "routeS" : 1, "routeW" : 3, "isShield" : false, "isEndOfRoad" : false, "isEndOfTown" : true, "isMonastery" : false}, {"id" : 8, "number" : 2, "routeN" : 1, "routeE" : 3, "routeS" : 3, "routeW" : 1, "isShield" : false, "isEndOfRoad" : false, "isEndOfTown" : true, "isMonastery" : false}, {"id" : 9, "number" : 3, "routeN" : 3, "routeE" : 2, "routeS" : 2, "routeW" : 1, "isShield" : false, "isEndOfRoad" : false, "isEndOfTown" : true, "isMonastery" : false}, {"id" : 10, "number" : 3, "routeN" : 2, "routeE" : 3, "routeS" : 1, "routeW" : 2, "isShield" : false, "isEndOfRoad" : false, "isEndOfTown" : true, "isMonastery" : false}, {"id" : 11, "number" : 3, "routeN" : 2, "routeE" : 3, "routeS" : 2, "routeW" : 2, "isShield" : false, "isEndOfRoad" : true, "isEndOfTown" : true, "isMonastery" : false}, {"id" : 12, "number" : 2, "routeN" : 3, "routeE" : 1, "routeS" : 1, "routeW" : 3, "isShield" : true, "isEndOfRoad" : false, "isEndOfTown" : false, "isMonastery" : false}, {"id" : 13, "number" : 3, "routeN" : 3, "routeE" : 1, "routeS" : 1, "routeW" : 3, "isShield" : false, "isEndOfRoad" : false, "isEndOfTown" : false, "isMonastery" : false}, {"id" : 14, "number" : 2, "routeN" : 3, "routeE" : 2, "routeS" : 2, "routeW" : 3, "isShield" : true, "isEndOfRoad" : false, "isEndOfTown" : false, "isMonastery" : false}, {"id" : 15, "number" : 3, "routeN" : 3, "routeE" : 2, "routeS" : 2, "routeW" : 3, "isShield" : false, "isEndOfRoad" : false, "isEndOfTown" : false, "isMonastery" : false}, {"id" : 16, "number" : 1, "routeN" : 3, "routeE" : 3, "routeS" : 1, "routeW" : 3, "isShield" : true, "isEndOfRoad" : false, "isEndOfTown" : false, "isMonastery" : false}, {"id" : 17, "number" : 3, "routeN" : 3, "routeE" : 3, "routeS" : 1, "routeW" : 3, "isShield" : false, "isEndOfRoad" : false, "isEndOfTown" : false, "isMonastery" : false}, {"id" : 18, "number" : 2, "routeN" : 3, "routeE" : 3, "routeS" : 2, "routeW" : 3, "isShield" : true, "isEndOfRoad" : true, "isEndOfTown" : false, "isMonastery" : false}, {"id" : 19, "number" : 1, "routeN" : 3, "routeE" : 3, "routeS" : 2, "routeW" : 3, "isShield" : false, "isEndOfRoad" : true, "isEndOfTown" : false, "isMonastery" : false}, {"id" : 20, "number" : 8, "routeN" : 2, "routeE" : 1, "routeS" : 2, "routeW" : 1, "isShield" : false, "isEndOfRoad" : false, "isEndOfTown" : false, "isMonastery" : false}, {"id" : 21, "number" : 9, "routeN" : 1, "routeE" : 1, "routeS" : 2, "routeW" : 2, "isShield" : false, "isEndOfRoad" : false, "isEndOfTown" : false, "isMonastery" : false}, {"id" : 22, "number" : 4, "routeN" : 1, "routeE" : 2, "routeS" : 2, "routeW" : 2, "isShield" : false, "isEndOfRoad" : true, "isEndOfTown" : false, "isMonastery" : false}, {"id" : 23, "number" : 1, "routeN" : 2, "routeE" : 2, "routeS" : 2, "routeW" : 2, "isShield" : false, "isEndOfRoad" : true, "isEndOfTown" : false, "isMonastery" : false}]'
+
+function initial() {
+    this.number = 0;
+    this.routeN = 0;
+    this.routeE = 0;
+    this.routeS = 0;
+    this.routeW = 0;
+    this.rotation = 0; // заглушка, использоваться не должна
+    this.isShield = false;
+    this.isEndOfRoad = false;
+    this.isEndOfTown = false;
+    this.isMonastery = false;
+}
+
+function init() {
+    let DATA = JSON.parse(data);
+    let initialValue = [];
+    for (let i = 0; i < 24; i++) {
+        initialValue[i] = new initial();
+        initialValue[i].number = DATA[i].number;
+        initialValue[i].routeN = DATA[i].routeN;
+        initialValue[i].routeE = DATA[i].routeE;
+        initialValue[i].routeS = DATA[i].routeS;
+        initialValue[i].routeW = DATA[i].routeW;
+        initialValue[i].isShield = DATA[i].isShield;
+        initialValue[i].isEndOfRoad = DATA[i].isEndOfRoad;
+        initialValue[i].isEndOfTown = DATA[i].isEndOfTown;
+        initialValue[i].isMonastery = DATA[i].isMonastery;
+    }
+    /*console.log(initialValue);*/
+    return initialValue;
+}
+
--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 let table = document.getElementById('table');
+let initValue = [0, 0, 1, 1, 1, 1, 2, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 6, 7, 7, 7, 8, 8, 9, 9, 9, 10, 10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 15, 16, 17, 17, 17, 18, 18, 19, 20, 20, 20, 20, 20, 20, 20, 20, 21, 21, 21, 21, 21, 21, 21, 21, 21, 22, 22, 22, 22, 23]
 
-
-const n = 15;
+const n = 11;
 const center = (n - 1)/2;
 
 function Cell(x, y) {
@@ -13,18 +13,37 @@ function Cell(x, y) {
     this.routeE = 0;   // 1 - field
     this.routeS = 0;   // 2 - road
     this.routeW = 0;   // 3 - town
-    function rotation() {
-        let buffer = this.routeN;
-        this.routeN = this.routeW;
-        this.routeW = this.routeS;
-        this.routeS = this.routeE;
-        this.routeE = buffer;
-    }
+    this.rotation = 0;
     this.isEndOfRoad = false;
     this.isMonastery = false;
     this.isShield = false;
     this.isEndOfTown = false;
 }
+
+function rotate(val) {
+    let buffer = val.routeN;
+    val.routeN = val.routeW;
+    val.routeW = val.routeS;
+    val.routeS = val.routeE;
+    val.routeE = buffer;
+    val.rotation = (val.rotation + 90) % 360;
+}
+
+
+/*let DATA = JSON.parse(data);
+
+for (let i = 0; i < 24; i++) {
+    initialValue[i].number = DATA[i].number;
+    initialValue[i].routeN = DATA[i].routeN;
+    initialValue[i].routeE = DATA[i].routeE;
+    initialValue[i].routeS = DATA[i].routeS;
+    initialValue[i].routeW = DATA[i].routeW;
+    initialValue[i].isShield = DATA[i].isShield;
+    initialValue[i].isEndOfRoad = DATA[i].isEndOfRoad;
+    initialValue[i].isEndOfTown = DATA[i].isEndOfTown;
+    initialValue[i].isMonastery = DATA[i].isMonastery;
+}*/
+let initialValue = init();
 
 function updateNeighbours(x, y, val) {
     if (++x < n) {
@@ -43,8 +62,6 @@ function updateNeighbours(x, y, val) {
         cellsJS[x * n + y].isNeighbourWithTile = val;
     }
 }
-
-
 
 function step() {
 
@@ -71,29 +88,119 @@ for (let i = 0; i < n; i++) {
             }
             cellsJS[i*n+j].isExistTile = !cellsJS[i*n+j].isExistTile;
             console.log(i, j);
-            checkField();
+            checkFieldColor();
         }
     }
 }
 
-let cellsHTML = document.getElementsByClassName("cell");  // Warning! cell - массив ячеек в html
-                                                                // cells - в js
-function checkField() {
+let cellsHTML = document.getElementsByClassName("cell");
+
+function checkFieldColor() {
+    let counter = 0;
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < n; j++) {
             if (cellsJS[i*n+j].isNeighbourWithTile && !cellsJS[i*n+j].isExistTile) {
                 cellsHTML[i*n+j].style['backgroundColor'] = '#6666'
+                counter++;
             }
         }
     }
+    return counter;
 }
 
-cellsJS[center*n+center].routeN = 3;
-cellsJS[center*n+center].routeE = 2; // id center tile - 7
+function checkField(obj) {  // Для копий объекта!
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (cellsJS[i*n+j].isNeighbourWithTile && !cellsJS[i*n+j].isExistTile) {
+                /*console.log('.');*/
+                for (let k = 0; k < 4; k++) {
+                    if (k > 0) {
+                        rotate(obj);
+                    }
+                    if (i > 0) {
+                        if (cellsJS[(i-1)*n+j].routeS !== 0 && cellsJS[(i-1)*n+j].routeS !== obj.routeN) {
+                            /*console.log(cellsJS[(i-1)*n+j].routeS, obj.routeN);*/
+                            continue;
+                        }
+                    }
+                    if (j > 0) {
+                        if (cellsJS[i*n+j-1].routeE !== 0 && cellsJS[i*n+j-1].routeE !== obj.routeW) {
+                            /*console.log(cellsJS[i*n+j-1].routeE, obj.routeW);*/
+                            continue;
+                        }
+                    }
+                    if (i < n) {
+                        if (cellsJS[(i+1)*n+j].routeN !== 0 && cellsJS[(i+1)*n+j].routeN !== obj.routeS) {
+                            /*console.log(cellsJS[(i+1)*n+j].routeN, obj.routeS);*/
+                            continue;
+                        }
+                    }
+                    if (j < n) {
+                        if (cellsJS[i*n+j+1].routeW !== 0 && cellsJS[i*n+j+1].routeW !== obj.routeE) {
+                            /*console.log(cellsJS[i*n+j+1].routeW, obj.routeE);*/
+                            continue;
+                        }
+                    }
+                    return true;
+                }
+
+            }
+        }
+    }
+    return false;
+}
+
+checkField(initialValue[0]);
+
+let controlArray = [];
+for (let i = 0; i < 24; i++) {
+    controlArray[i] = 0;
+}
+
+function randomize() {
+    let randomizeArray = [];
+    let k = 0;
+    for (let i = 0; i < 24; i++) {
+        if (initialValue[i].number > 0) {
+            if (checkField(Object.assign(initialValue[i]))) {
+                for (let j = 0; j < initialValue[i].number; j++) {
+                    randomizeArray[k++] = i;
+                    /*console.log(randomizeArray);*/
+                }
+            }
+        }
+    }
+    if (k === 0) {
+        return;         // Подходящие плитки закончились -> выход из программы, окончательный подсчет очков
+    }
+    let b = randomizeArray[Math.trunc(Math.random() * k)];
+    controlArray[b]++;
+    /*console.log(b);*/
+    initialValue[b].number--;
+}
+
+
+
+
+
+cellsJS[center*n+center].routeN = 2;
+cellsJS[center*n+center].routeE = 3; // id center tile - 3
 cellsJS[center*n+center].routeW = 2;
 cellsJS[center*n+center].routeS = 1;
+for (let i = 0; i < 3; i++) {
+    rotate(cellsJS[center * n + center]);
+}
+cellsHTML[center * n + center].style.transform = 'rotate(cellsJS[center*n+center].rotation)';
+cellsHTML[center * n + center].style.backgroundImage = 'url("Tiles/3.png")';
 
 updateNeighbours(center,center, true);
 
-checkField();
+checkFieldColor(3);
 
+/*randomize();*/
+
+for (let h = 0; h < 100; h++) {
+    randomize();
+}
+
+console.log(controlArray);
